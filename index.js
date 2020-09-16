d97666b8fdddfeb49d962331fec2873039928f27
azu <azuciao@gmail.com>
2020-09-16 22:44:56 +0900

    1MB js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const crypto = require("crypto");
 Array.from({ length: 25 }).forEach((_, i) => {
     const id = crypto.randomBytes(256).toString('hex');
-    const text = Array.from({ length: 16 }).map(() => {
+    const text = Array.from({ length: 108 }).map(() => {
         return crypto.randomBytes(1024).toString('hex')
     }).join("__")
     fs.writeFileSync(path.join(__dirname, "script", i + ".js"), `
