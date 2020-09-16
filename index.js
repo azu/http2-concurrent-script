b6488fcc622527fe98c5eb6e30a1d4c9892aff48
azu <azuciao@gmail.com>
2020-09-16 22:24:49 +0900

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
+Array.from({ length: 25 }).forEach((_, i) => {
+    const id = crypto.randomBytes(256).toString('hex');
+    const text = Array.from({ length: 16 }).map(() => {
+        return crypto.randomBytes(1024).toString('hex')
+    }).join("__")
+    fs.writeFileSync(path.join(__dirname, "script", i + ".js"), `
+const text = "${text}";
+console.log("Load ${i}.js");
+    `, "utf-8")
+    console.log(`<script async src="script/${i}.js"></script>`)
+})
